a6565fdafde9d9c5239e571156e653d6103847e1
Lindsay Holmwood <lindsay@holmwood.id.au>
2009-08-31 01:59:08 +0200

    added some example code to render a load graph

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,9 +1,86 @@
-// Creates canvas 320 × 200 at 10, 50
-var paper = Raphael(10, 50, 320, 200);
-// Creates circle at x = 50, y = 40, with radius 10
-var circle = paper.circle(50, 40, 10);
-// Sets the fill attribute of the circle to red (#f00)
-circle.attr("fill", "#f00");
-// Sets the stroke attribute of the circle to white (#fff)
-circle.attr("stroke", "#fff");
+window.addEvent('domready', function () {
+
+	var graphData = function(data) {
+	  var host = 'theodor';
+		var plugin = 'load';
+		var plugin_instance = 'load';
+		var stats = data[host][plugin][plugin_instance];
+
+		var startTime = stats.splice(0,1);
+		var endTime = stats.splice(0,1);
+		var labels = stats.splice(0,1)[0];
+		var dataSet = stats.splice(0,1);
+ 
+		organisedDataSet = new Hash();
+		labels.each(function(label, index) {
+			blob = new Hash();
+			blob.set('data', dataSet[0].map(function(item) { 
+					return isNaN(item[index]) ? 0 : item[index] 
+			}));
+			blob.set('path', r.path({"stroke": colours[plugin][plugin_instance][label] || "#000", 
+					                     "stroke-width": 1.5}));
+
+			blob.set('min', Math.min.apply(Math, blob.get('data')));
+			blob.set('max', Math.max.apply(Math, blob.get('data')));
+			organisedDataSet.set(label, blob);
+		});
+
+		var length = organisedDataSet.get(labels[0]).get('data').length;
+
+		var minX = leftEdge;
+		var maxX = leftEdge / 4 + gridWidth;
+		var minY = topEdge + gridHeight;
+		var maxY = topEdge;
+
+		organisedDataSet.each(function(blob, key) {
+      blob['data'].each(function(value, index) {
+  			x = minX + index / length * maxX;
+	  		y = minY - value * maxY * maxY;
+		  	blob['path'][index == 0 ? "moveTo" : "cplineTo"](x, y);
+			});
+		});
+
+	};
+
+	var colours = {'load': {'load': {'shortterm': "#73d216", 
+		                               'midterm': "#3465a4",
+												  				 'longterm': "#ef2929"}},
+			           'memory': {'memory-free': {'value': '#75507b'}}
+	}
+
+	var request = new Request.JSON({
+    url: '/data/theodor/load/load',
+		secure: false,
+		onComplete: function(responseJSON, responseText) {
+		  graphData(responseJSON);
+		}
+	});
+
+	var width = 900, 
+	    height = 480,
+			leftEdge = 10,
+			topEdge = 10,
+			gridWidth = 880,
+			gridHeight = 200,
+			columns = 60,
+			rows = 8,
+			topGutter = 50,
+			gridBorderColour = '#ccc';
+
+	var r = Raphael("holder", width, height);
+
+	r.drawGrid(
+			leftEdge, 
+			topEdge, 
+			gridWidth, 
+			gridHeight,
+			columns, 
+			rows, 
+			gridBorderColour
+	);
+
+	var time = (new Date).getTime() / 1000;
+	request.get({'start': time - 3600 , 'end': time});
+	
+});
 
