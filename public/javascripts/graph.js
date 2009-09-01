2cac97525927f076e8003d65f46685bc761e7161
Lindsay Holmwood <lindsay@holmwood.id.au>
2009-09-01 03:57:49 +0200

    added another class to display multiple entries on a graph

--- public/javascripts/graph.js
+++ public/javascripts/graph.js
@@ -1,12 +1,9 @@
 var collectdSingleGraph = new Class({
     Implements: [Options, Events],
         options: {
-            host: 'theodor',
-            plugin: 'load',
-            plugin_instance: 'load',
             width: 900,
             height: 220,
-            leftEdge: 10,
+            leftEdge: 100,
             topEdge: 10,
             gridWidth: 670,
             gridHeight: 200,
@@ -16,9 +13,11 @@ var collectdSingleGraph = new Class({
             gridBorderColour: '#ccc',
             secureJSON: false
     },
-    initialize: function(element, options) {
+    initialize: function(element, host, plugin, options) {
         this.element = element;
         this.setOptions(options);
+				this.options.host = host;
+				this.options.plugin = plugin;
         this.canvas = Raphael(element, this.options.width, this.options.height);
         this.getData(); // calls graphData
     },
@@ -37,7 +36,6 @@ var collectdSingleGraph = new Class({
         this.request.get();
     },
     graphData: function(data) {
-
         this.stats = data[this.options.host][this.options.plugin][this.options.plugin_instance];
         this.startTime = this.stats.splice(0,1);
         this.endTime = this.stats.splice(0,1);
@@ -71,7 +69,7 @@ var collectdSingleGraph = new Class({
         });
   
         this.canvas.g.txtattr.font = "11px 'sans-serif'";
-        this.canvas.g.linechart(20, 10, this.options.gridWidth, this.options.gridHeight, x, y, {
+        this.canvas.g.linechart(this.options.leftEdge, this.options.topEdge, this.options.gridWidth, this.options.gridHeight, x, y, {
             nostroke: false, shade: false, width: 1.5,
             axis: "0 0 1 1", axisxlabels: 'head', axisxstep: 10,
             colors: colours
@@ -79,7 +77,8 @@ var collectdSingleGraph = new Class({
   
         this.buildLabels(this.labels)
     },
-    buildLabels: function(labels) {
+    
+		buildLabels: function(labels) {
         labels.each(function(label) {
             container = new Element('div', {
                 'class': 'label plugin',
@@ -96,6 +95,86 @@ var collectdSingleGraph = new Class({
             desc = new Element('span', {
                 'class': 'label plugin description ' + label,
                 'html': label
+            });
+        
+            container.grab(box);
+            container.grab(desc);
+            $(this.element).getChildren('div.labels')[0].grab(container);
+
+        },this);    
+    }
+
+});
+
+var collectdMultiGraph = new Class({
+		Extends: collectdSingleGraph,
+    graphData: function(data) {
+
+				this.plugin_instances = new Hash()
+
+        $each(data[this.options.host][this.options.plugin], function(data, plugin_instance) {
+
+            stats = data;
+            startTime = stats.splice(0,1);
+            endTime = stats.splice(0,1);
+            labels = stats.splice(0,1)[0];
+            dataSet = stats.splice(0,1);
+
+		        structuredDataSet = new Hash()
+		        labels.each(function(label, index) {
+		            blob = new Hash()
+		            blob.set('data', dataSet[0].map(function(item) {
+		                return isNaN(item[index]) ? 0 : item[index]
+		            }));
+		            blob.set('min', Math.min.apply(Math, blob.get('data')));                              
+		            blob.set('max', Math.max.apply(Math, blob.get('data')));
+		            blob.set('colour', this.options.colours[this.options.plugin][plugin_instance][label]);
+		            structuredDataSet.set(label, blob);
+		        }, this);
+
+						this.plugin_instances.set(plugin_instance, structuredDataSet);
+						this.length = structuredDataSet.value.get('data').length
+		           
+				}, this);
+
+				var x = [];
+        for (var i = 0; i < this.length; i++) {
+            x[i] = i * this.options.gridWidth / this.length;
+        }
+
+        y = []
+        colours = []
+				this.plugin_instances.each(function(data, name) {
+        		y.include(data.value.get('data'));
+						colours.include(data.value.get('colour'));
+				})
+        
+				this.canvas.g.txtattr.font = "11px 'sans-serif'";
+        this.canvas.g.linechart(this.options.leftEdge, this.options.topEdge, this.options.gridWidth, this.options.gridHeight, x, y, {
+            nostroke: false, shade: false, width: 1.5,
+            axis: "0 0 1 1", axisxlabels: 'head', axisxstep: 10,
+            colors: colours
+        });
+  
+        this.buildLabels(this.plugin_instances)
+		},
+		buildLabels: function(plugin_instances) {
+        plugin_instances.each(function(data, name) {
+            container = new Element('div', {
+                'class': 'label plugin',
+            });
+
+            box = new Element('span', {
+                'class': 'label plugin box ' + name,
+                'html': '&nbsp;',
+                'styles': { 
+                      'background-color': data.value.get('colour')
+                }
+            });
+        
+            desc = new Element('span', {
+                'class': 'label plugin description ' + name,
+                'html': name.split('-')[1]
             });
         
             container.grab(box);
