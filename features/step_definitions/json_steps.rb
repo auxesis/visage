0dcb9d9d0a309f70bead2bcd7218c1f617d1d26e
Lindsay Holmwood <lindsay@holmwood.id.au>
2009-09-05 00:39:20 +0200

    added pending step definitions, as the Yajl bug is blocking these

--- features/step_definitions/json_steps.rb
+++ features/step_definitions/json_steps.rb
@@ -4,3 +4,20 @@ Then /^I should receive valid JSON$/ do
     yajl.parse(response_body)
   }.should_not raise_error
 end
+
+Then /^I should receive JSON wrapped in a callback named "([^\"]*)"$/ do |callback|
+  response_body.should =~ /^#{callback}\(.+\)$/
+end
+
+Then /^the JSON should have a plugin instance named "([^\"]*)"$/ do |arg1|
+  pending
+end
+
+Then /^the JSON should have a plugin named "([^\"]*)"$/ do |arg1|
+  pending
+end
+
+Then /^the JSON should have multiple plugin instances under the "([^\"]*)" plugin$/ do |arg1|
+  pending
+end
+
