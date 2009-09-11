7dae4effbbc1a383be8f4922c9dec88f02dedc1f
Lindsay Holmwood <lindsay@holmwood.id.au>
2009-09-11 14:13:59 +0200

    removed extraneous error checking, pass the profiles in directly

--- lib/collectd-profile.rb
+++ lib/collectd-profile.rb
@@ -11,26 +11,20 @@ class CollectdProfile
 
   class << self
 
-    attr_accessor :config_filename
+    attr_accessor :profiles
 
     def get(id)
       id.gsub!(/\s+/, '+')
-      if @config_filename && File.exists?(@config_filename)
-        config = YAML::load(File.read(@config_filename))
-        OpenStruct.new(config['profiles'][id])
-      end
+      OpenStruct.new(@profiles[id])
     end
 
     def all
-      if @config_filename && File.exists?(@config_filename)
-        config = YAML::load(File.read(@config_filename))
-        # here be ugliness
-        profiles = config['profiles'].to_a.sort_by { |profile| 
-          profile[1]["order"] 
-        }.map { |profile| 
-          OpenStruct.new(profile[1].merge({'url' => profile[0]}))
-        }
-      end
+      # here be ugliness
+      profiles = @profiles.to_a.sort_by { |profile| 
+        profile[1]["order"] 
+      }.map { |profile| 
+        OpenStruct.new(profile[1].merge({'url' => profile[0]}))
+      }
     end
 
   end
