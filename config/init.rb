05b677ba315953d867aebd85825e409cd4dd7514
Lindsay Holmwood <lindsay@holmwood.id.au>
2009-09-11 14:11:39 +0200

    renamed variables

--- config/init.rb
+++ config/init.rb
@@ -2,10 +2,10 @@ __DIR__ = File.expand_path(File.join(File.dirname(__FILE__)))
 require File.join(__DIR__, '..', 'lib', 'visage-config')
 
 Visage::Config.use do |c|
-  c['color_mappings'] = YAML::load(File.read(File.join(__DIR__, 'colors.yaml')))
+  c['fallback_colors'] = YAML::load(File.read(File.join(__DIR__, 'colors.yaml')))
  
-  c['config_filename'] = File.expand_path(File.join(__DIR__, 'config.yaml'))
-  YAML::load(File.read(c['config_filename'])).each_pair do |key, value|
+  config_filename = File.expand_path(File.join(__DIR__, 'config.yaml'))
+  YAML::load(File.read(config_filename)).each_pair do |key, value|
     c[key] = value
   end
 
