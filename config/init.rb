f97ae624aa24000b9cff291099587a2113e4ee63
Lindsay Holmwood <lindsay@holmwood.id.au>
2009-09-11 15:12:13 +0200

    cleaned up config structure

--- config/init.rb
+++ config/init.rb
@@ -4,8 +4,13 @@ require File.join(__DIR__, '..', 'lib', 'visage-config')
 Visage::Config.use do |c|
   c['fallback_colors'] = YAML::load(File.read(File.join(__DIR__, 'colors.yaml')))
  
-  config_filename = File.expand_path(File.join(__DIR__, 'config.yaml'))
-  YAML::load(File.read(config_filename)).each_pair do |key, value|
+  profile_filename = File.join(__DIR__, 'profiles.yaml')
+  YAML::load(File.read(profile_filename)).each_pair do |key, value|
+    c[key] = value
+  end
+
+  plugin_colors_filename = File.join(__DIR__, 'plugin-colors.yaml')
+  YAML::load(File.read(plugin_colors_filename)).each_pair do |key, value|
     c[key] = value
   end
 
