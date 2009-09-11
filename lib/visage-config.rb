6f02152ec8dc85113346007c335170da61e71651
Lindsay Holmwood <lindsay@holmwood.id.au>
2009-09-11 13:32:44 +0200

    added class for holding visage config

---
+++ lib/visage-config.rb
@@ -0,0 +1,24 @@
+module Visage
+  class Config 
+    class << self
+
+      def use
+        @configuration ||= {}
+        yield @configuration
+        nil
+      end
+
+      def method_missing(method, *args)
+        if method.to_s[-1,1] == '='
+          @configuration[method.to_s.tr('=','')] = *args
+        else
+          @configuration[method.to_s]
+        end
+      end
+
+      def to_hash
+        @configuration
+      end
+    end
+  end
+end
