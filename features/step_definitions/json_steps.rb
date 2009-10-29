ffa7ead2de07a34c1f685fa79fe97074a4d40984
Lindsay Holmwood <lindsay@holmwood.id.au>
2009-10-29 13:04:12 +0100

    filled in steps for verifying json works as expected

--- features/step_definitions/json_steps.rb
+++ features/step_definitions/json_steps.rb
@@ -9,15 +9,44 @@ Then /^I should receive JSON wrapped in a callback named "([^\"]*)"$/ do |callba
   response_body.should =~ /^#{callback}\(.+\)$/
 end
 
-Then /^the JSON should have a plugin instance named "([^\"]*)"$/ do |arg1|
-  pending
+Then /^the JSON should have a plugin instance named "([^\"]*)"$/ do |plugin_instance|
+  yajl = Yajl::Parser.new
+  data = yajl.parse(response_body)
+
+  data.values.map { |k,v| k.values }.map {|k,v| k.keys }.flatten.include?(plugin_instance).should be_true
 end
 
-Then /^the JSON should have a plugin named "([^\"]*)"$/ do |arg1|
-  pending
+Then /^the JSON should have a plugin named "([^\"]*)"$/ do |plugin|
+  yajl = Yajl::Parser.new
+  data = yajl.parse(response_body)
+
+  data.values.map { |k,v| k.keys }.flatten.include?(plugin).should be_true
 end
 
 Then /^the JSON should have multiple plugin instances under the "([^\"]*)" plugin$/ do |arg1|
-  pending
+  yajl = Yajl::Parser.new
+  data = yajl.parse(response_body)
+
+  data.values.map { |k,v| k[arg1] }.map { |k,v| k.keys }.flatten.size.should > 1
+end
+
+Then /^each plugin instance should have a different color$/ do
+  yajl = Yajl::Parser.new
+  data = yajl.parse(response_body)
+
+  @colours = []
+  data.values.map { |k,v| k.values }.map {|k,v| k.values }.map do |a|
+    a.each do |b|
+      b.last.should =~ /^#[0-9a-fA-F]+$/
+      @colours << b.last
+    end
+  end
+
+  @colours.uniq.size.should == @colours.size
+
+end
+
+Then /^the plugin instance should have a color$/ do
+  Then "each plugin instance should have a different color"
 end
 
