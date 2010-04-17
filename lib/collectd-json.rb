638f0b9010d48c998ba5d0364553105c5f13f017
Lindsay Holmwood <lindsay@holmwood.id.au>
2010-04-18 01:05:48 +1000

    refactored color detection method to be recursive and performant

--- lib/collectd-json.rb
+++ lib/collectd-json.rb
@@ -91,49 +91,16 @@ class CollectdJSON
   # doesn't try and have to work it out. This lets us use all sorts of funky
   # fallback logic when determining what colours should be used.
   def color_for(opts={})
-    case 
-    when @colors[opts[:plugin]] && @colors[opts[:plugin]][opts[:instance]] && @colors[opts[:plugin]][opts[:instance]][opts[:metric]]
-      color = @colors[opts[:plugin]][opts[:instance]].values.first
-      color ? color : fallback_color
 
-    when @colors[opts[:plugin]] && @colors[opts[:plugin]][opts[:instance]]
-      color = @colors[opts[:plugin]][opts[:instance]]
-      color ? color : fallback_color
+    plugin   = opts[:plugin]
+    instance = opts[:instance]
+    metric   = opts[:metric]
 
-    when opts[:plugin] =~ /\-/ && opts[:instance] =~ /\-/
-      base_plugin = opts[:plugin].split('-').first
-      base_instance = opts[:instance].split('-').first
-
-      if plugin_colors = @colors[base_plugin]
-        color = plugin_colors[opts[:instance]].values.first
-        color ? color : fallback_color
-      elsif plugin_colors = @colors[opts[:plugin]]
-        color = plugin_colors[base_instance]
-        color ? color : fallback_color
-      else
-        fallback_color
-      end
-
-    when opts[:instance] =~ /\-/
-      base_instance = opts[:instance].split('-').first
-      if plugin_colors = @colors[opts[:plugin]]
-        color = plugin_colors[base_instance]
-        color ? color : fallback_color
-      else
-        fallback_color
-      end
-
-    when opts[:plugin] =~ /\-/
-      base_plugin = opts[:plugin].split('-').first
-      if plugin_colors = @colors[base_plugin]
-        color = plugin_colors[opts[:instance]]
-      else
-        fallback_color
-      end
-
-    else
-      fallback_color
-    end
+    return fallback_color unless plugin
+    return color_for(opts.merge(:plugin => plugin[/(.+)-.+$/, 1])) unless @colors[plugin]
+    return color_for(opts.merge(:instance => instance[/(.+)-.+$/, 1])) unless @colors[plugin][instance]
+    return @colors[plugin][instance][metric] if @colors[plugin][instance]
+    return fallback_color
   end
 
   def fallback_color
