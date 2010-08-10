860e3b99dab8c03d6560026ca20965294d06394a
Lindsay Holmwood <lindsay@holmwood.id.au>
2010-08-11 08:12:06 +1000

    make the pipe persist across steps, added step to verify string exists in output

--- features/step_definitions/visage_steps.rb
+++ features/step_definitions/visage_steps.rb
@@ -6,15 +6,20 @@ When /^I start the visage server helper with "([^"]*)"$/ do |cmd|
   @root = Pathname.new(File.dirname(__FILE__)).parent.parent.expand_path
   command = "#{@root.join('bin')}/#{cmd}"
 
-  pipe = IO.popen(command)
+  @pipe = IO.popen(command, "r")
   sleep 2 # so the visage server has a chance to boot
 
   # clean up the visage server when the tests finish
   at_exit do
-    Process.kill("KILL", pipe.pid)
+    Process.kill("KILL", @pipe.pid)
   end
 end
 
 Then /^a visage web server should be running$/ do
   `ps -eo cmd |grep ^visage`.size.should > 0
+end
+
+Then /^I should see "([^"]*)" on the terminal$/ do |string|
+  output = @pipe.read(250)
+  output.should =~ /#{string}/
 end
