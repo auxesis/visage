ae1b2e2cfcb8940e111cb84192dd1ba0a8102b40
Lindsay Holmwood <lindsay@holmwood.id.au>
2010-08-19 22:24:12 +1000

    added javascript for all the cutesy sliders/togglers

--- javascripts/index.js
+++ javascripts/index.js
@@ -1,1 +1,32 @@
-alert('hi');
+window.addEvent('load', function() {
+    var debianInstallerSlider  = new Fx.Slide('debian-installer', {'duration': 300, 'transition': Fx.Transitions.Sine});
+
+    /* the rhell installer instructions slider/toggler combo */
+    var rhelInstallerSlider  = new Fx.Slide('rhel-installer', {'duration': 300});
+    var installerToggler = $('installer-toggler');
+
+    installerToggler.addEvent('click', function(e) {
+        e.stop();
+        rhelInstallerSlider.toggle();
+        debianInstallerSlider.toggle();
+
+        if (installerToggler.get('html').contains('RHEL')) {
+            installerToggler.set('html', 'Nah, just kidding - I use <a href="#debian-installer">Debian/Ubuntu</a>.');
+        } else {
+            installerToggler.set('html', 'Hey wait a sec! I use <a href="#rhel-installer">RHEL/CentOS</a>!');
+        }
+    });
+
+    rhelInstallerSlider.hide();
+
+    /* the path hack slider/toggler combo */
+    var pathHackSlider  = new Fx.Slide('rubygems-path-hack', {'duration': 300});
+    var pathHackToggler = $('rubygems-path-hack-toggler');
+
+    pathHackToggler.addEvent('click', function(e) {
+      e.stop();
+      pathHackSlider.slideIn();
+    });
+
+    pathHackSlider.hide();
+});
