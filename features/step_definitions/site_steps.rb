141cd7e5d755acc25f2dff12a4d21ae562374873
Lindsay Holmwood <lindsay@holmwood.id.au>
2010-09-26 13:41:55 +1000

    filled out steps

--- features/step_definitions/site_steps.rb
+++ features/step_definitions/site_steps.rb
@@ -14,3 +14,14 @@ Then /^I should see a list of profiles$/ do
   doc = Nokogiri::HTML(response_body)
   doc.search('div#profiles ul li').size.should > 1
 end
+
+Then /^I should see a list of profiles sorted alphabetically$/ do
+  doc = Nokogiri::HTML(response_body)
+  profiles = doc.search('div#profiles ul li')
+  profiles.size.should > 1
+
+  unsorted = profiles.map { |p| p.text.strip }
+  sorted = profiles.map { |p| p.text.strip }.sort
+
+  unsorted.should == sorted
+end
