b4eb14d309b11558e3f33841dddd0c4360b08d2a
Lindsay Holmwood <lindsay@holmwood.id.au>
2011-05-07 09:41:40 +1000

    Cleaned up trailing whitespace

--- lib/visage-app/patches.rb
+++ lib/visage-app/patches.rb
@@ -1,10 +1,10 @@
-# extracted from Extlib. 
+# extracted from Extlib.
 # FIXME: what's the licensing here?
 class String
-  def camel_case                                                                                                                                    
-    return self if self !~ /_/ && self =~ /[A-Z]+.*/                                                                                                
-    split('_').map{|e| e.capitalize}.join                                                                                                           
-  end   
+  def camel_case
+    return self if self !~ /_/ && self =~ /[A-Z]+.*/
+    split('_').map{|e| e.capitalize}.join
+  end
 
   def blank?
     strip.empty?
