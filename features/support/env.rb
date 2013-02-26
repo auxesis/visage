732fc256c59a9f54aac96d8986b7f3dc4c3f827e
Lindsay Holmwood <lindsay@holmwood.id.au>
2013-02-27 00:24:20 +1100

    Fill out mock backend. Make tests use mock backend.

--- features/support/env.rb
+++ features/support/env.rb
@@ -8,11 +8,12 @@ require 'capybara/cucumber'
 require 'capybara/poltergeist'
 
 # Application setup
-
 root     = Pathname.new(File.dirname(__FILE__)).parent.parent.expand_path
 app_file = root.join('lib/visage-app').to_s
+ENV['CONFIG_PATH']         = root.join('features/support/config/default').to_s
+# use a Mock backend, so tests don't depend on any specific backend (e.g. RRD)
+ENV['VISAGE_DATA_BACKEND'] = 'Mock'
 require(app_file)
-ENV['CONFIG_PATH'] = root.join('features/support/config/default').to_s
 
 # http://opensoul.org/blog/archives/2010/05/11/capybaras-eating-cucumbers/
 Capybara.app = Rack::Builder.new do
