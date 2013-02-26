4bef2c8ba438bfcfa489424f4553d08ceacb8cd9
Lindsay Holmwood <lindsay@holmwood.id.au>
2013-02-26 22:35:57 +1100

    Streamline and refactor RRD backend
     - Merge Visage::Collectd::RRDs into Visage::Collectd::JSON, to simplify
       code paths and consolidate backend access
     - Rename Visage::Collectd::JSON to Visage::Data, as it more accurately
       reflects what the class is meant to be doing.
     - Move duck punches into visage-app/patches, so they're all listed in
       one place.
     - Re-indent code.

--- lib/visage-app/patches.rb
+++ lib/visage-app/patches.rb
@@ -16,3 +16,46 @@ class NilClass
     true
   end
 end
+
+class Array
+  def in_groups(number, fill_with = nil)
+    raise "Error - in_groups of zero doesn't make sense" unless number > 0
+    # size / number gives minor group size;
+    # size % number gives how many objects need extra accomodation;
+    # each group hold either division or division + 1 items.
+    division = size / number
+    modulo = size % number
+
+    # create a new array avoiding dup
+    groups = []
+    start = 0
+
+    number.times do |index|
+      length = division + (modulo > 0 && modulo > index ? 1 : 0)
+      padding = fill_with != false &&
+        modulo > 0 && length == division ? 1 : 0
+      groups << slice(start, length).concat([fill_with] * padding)
+      start += length
+    end
+
+    if block_given?
+      groups.each{|g| yield(g) }
+    else
+      groups
+    end
+  end
+
+  def sum
+    inject( nil ) { |sum,x| sum ? sum+x : x }
+  end
+
+  def mean
+    if size > 0
+      sum / size
+    else
+      nil
+    end
+  end
+
+end
+
