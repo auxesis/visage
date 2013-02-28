e05e0c5fc3311ff35b8e34537470a677264fe53d
Lindsay Holmwood <lindsay@holmwood.id.au>
2013-02-28 23:29:10 +1100

    Add read_until_timeout method, to read from a named pipe without blocking tests

--- features/support/daemons.rb
+++ features/support/daemons.rb
@@ -57,6 +57,31 @@ def spawn_daemon(command, opts={})
   daemon
 end
 
+# Testing daemons with Ruby backticks blocks indefinitely, because the
+# backtick method waits for the program to exit. We use the select() system
+# call to read from a pipe connected to a daemon, and return if no data is
+# read within the specified timeout.
+#
+# http://weblog.jamisbuck.org/assets/2006/9/25/gdb.rb
+def read_until_timeout(pipe, timeout=1, verbose=false)
+  output = []
+  line    = ""
+  while data = IO.select([pipe], nil, nil, timeout) do
+    next if data.empty?
+    char = pipe.read(1)
+    break if char.nil?
+
+    line << char
+    if line[-1] == ?\n
+      puts line if verbose
+      output << line
+      line = ""
+    end
+  end
+
+  output
+end
+
 at_exit do
   kill_lingering_daemons
 end
