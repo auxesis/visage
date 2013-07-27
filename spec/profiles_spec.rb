456b6d7533ed6045c6d760af87307f3c8bf54f13
Lindsay Holmwood <lindsay@holmwood.id.au>
2013-07-28 00:04:11 +1000

    Reorganise grouping of tests

--- spec/profiles_spec.rb
+++ spec/profiles_spec.rb
@@ -2,9 +2,10 @@ require 'spec_helper'
 require 'visage-app/models/profile'
 
 describe "Profile" do
+
   describe "lookup" do
     it "should provide filtering for anonymous profiles" do
-      profiles = Visage::Profile.all(:anonymous => true)
+      profiles = Profile.all(:anonymous => true)
       profiles.size.should > 0
       profiles.each do |profile|
         profile.anonymous.should be_true
@@ -12,7 +13,7 @@ describe "Profile" do
     end
 
     it "should provide filtering for named profiles" do
-      profiles = Visage::Profile.all(:anonymous => false)
+      profiles = Profile.all(:anonymous => false)
       profiles.size.should > 0
       profiles.each do |profile|
         profile.anonymous.should be_false
@@ -20,85 +21,56 @@ describe "Profile" do
     end
 
     it "should return all records on unscoped lookups" do
-      anonymous = Visage::Profile.all(:anonymous => true).size
-      named     = Visage::Profile.all(:anonymous => false).size
-      all       = Visage::Profile.all.size
+      anonymous = Profile.all(:anonymous => true).size
+      named     = Profile.all(:anonymous => false).size
+      all       = Profile.all.size
 
       (anonymous + named).should == all
     end
 
     it "should allow sorting on name" do
-      default_sorting = Visage::Profile.all
-      custom_sorting  = Visage::Profile.all(:sort => :name)
+      default_sorting = Profile.all
+      custom_sorting  = Profile.all(:sort => :name)
 
       default_sorting.should_not == custom_sorting
     end
 
     it "should allow sorting on created_at" do
-      default_sorting = Visage::Profile.all
-      custom_sorting = Visage::Profile.all(:sort => :created_at)
+      default_sorting = Profile.all
+      custom_sorting = Profile.all(:sort => :created_at)
 
       default_sorting.should_not == custom_sorting
     end
 
     it "should allow ordering of records ascending or descending" do
-      ascending  = Visage::Profile.all(:sort => :id, :order => :ascending)
-      descending = Visage::Profile.all(:sort => :id, :order => :descending)
+      ascending  = Profile.all(:sort => :id, :order => :ascending)
+      descending = Profile.all(:sort => :id, :order => :descending)
 
       ascending.reverse.should == descending
     end
   end
 
-  it "should materialise a single profile" do
-    profile = Visage::Profile.get('aaa')
-    profile.should_not be_nil
-  end
-
-  it "should save a single profile" do
-    attributes = {
-      :anonymous => true,
-      :name      => 'An example profile',
-      :graphs    => [
-        { :plugin => 'memory', :host => 'foo', :start => Time.now.to_i },
-        { :plugin => 'memory', :host => 'bar', :start => Time.now.to_i },
-      ]
-    }
-
-    profile = Visage::Profile.new(attributes)
-    profile.save.should be_true
-  end
-
-  it "should automatically generate an id for a profile on save" do
-    attributes = {
-      :anonymous => true,
-      :name      => 'Another example profile',
-      :graphs => [
-        { :plugin => 'memory', :host => 'foo', :start => Time.now.to_i },
-        { :plugin => 'memory', :host => 'bar', :start => Time.now.to_i },
-      ]
-    }
-
-    profile = Visage::Profile.new(attributes)
-    profile.save.should be_true
-    profile.id.should_not be_nil
-  end
-
-  describe "validations and callbacks" do
-    it "should confirm presence of name" do
-      attributes = {}
-      profile = Visage::Profile.new(attributes)
-      profile.save.should_not be_true
-      profile.errors.should_not be_nil
+  describe "records" do
+    it "should be fetchable by id" do
+      profile = Profile.get('aaa')
+      profile.should_not be_nil
     end
 
-    it "should confirm presence of graphs" do
-      attributes = {}
-      profile = Visage::Profile.new(attributes)
-      profile.save.should_not be_true
-      profile.errors.should_not be_nil
+    it "should be saveable" do
+      attributes = {
+        :anonymous => true,
+        :name      => 'An example profile',
+        :graphs    => [
+          { :plugin => 'memory', :host => 'foo', :start => Time.now.to_i },
+          { :plugin => 'memory', :host => 'bar', :start => Time.now.to_i },
+        ]
+      }
+
+      profile = Profile.new(attributes)
+      profile.save.should be_true
     end
 
-    it "should automatically set created_at on create" do
+    it "should automatically generate an id for a profile on save" do
       attributes = {
         :anonymous => true,
         :name      => 'Another example profile',
@@ -108,15 +80,45 @@ describe "Profile" do
         ]
       }
 
-      profile = Visage::Profile.new(attributes)
+      profile = Profile.new(attributes)
+      profile.save.should be_true
+      profile.id.should_not be_nil
+    end
+
+    it "should be representable as JSON" do
+      profile = Profile.all.first
+      profile.to_json.should_not be_nil
+    end
+  end
+
+  describe "validations and callbacks" do
+    it "should confirm presence of name" do
+      attributes = {}
+      profile = Profile.new(attributes)
+      profile.save.should_not be_true
+      profile.errors.should_not be_nil
+    end
+
+    it "should confirm presence of graphs" do
+      attributes = {}
+      profile = Profile.new(attributes)
+      profile.save.should_not be_true
+      profile.errors.should_not be_nil
+    end
+
+    it "should automatically set created_at" do
+      attributes = {
+        :anonymous => true,
+        :name      => 'Another example profile',
+        :graphs => [
+          { :plugin => 'memory', :host => 'foo', :start => Time.now.to_i },
+          { :plugin => 'memory', :host => 'bar', :start => Time.now.to_i },
+        ]
+      }
+
+      profile = Profile.new(attributes)
       profile.save.should be_true
       profile.created_at.should_not be_nil
     end
   end
-
-  it "should represent a single profile as JSON" do
-    profile = Visage::Profile.all.first
-    profile.to_json.should_not be_nil
-  end
-
 end
